e74c6023908dbba5dba37cdc6b23dc09f02fd9fb
Vince <mvincem@gmail.com>
2021-03-25 17:44:53 +0100

    page liste dynamique

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
+import homeScreen from "./homeScreen.js"
 
-const cameraRoutes = require('./routes/camera');
-const teddyRoutes = require('./routes/teddy');
-const furnitureRoutes = require('./routes/furniture');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const path = require('path');
 
-const app = express();
+// const cameraRoutes = require('./routes/camera');
+// const teddyRoutes = require('./routes/teddy');
+// const furnitureRoutes = require('./routes/furniture');
 
-mongoose.connect(
-  'mongodb+srv://will:nAcmfCoHGDgzrCHG@cluster0-pme76.mongodb.net/test?retryWrites=true',
-  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Successfully connected to MongoDB Atlas!');
-  })
-  .catch((error) => {
-    console.log('Unable to connect to MongoDB Atlas!');
-    console.error(error);
-  });
+// const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+const router = () => {
+  const main = document.getElementById("main-container")
+  main.innerHTML = homeScreen.render();
+}
+window.addEventListener("load", router)
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+// mongoose.connect(
+//   'mongodb+srv://will:nAcmfCoHGDgzrCHG@cluster0-pme76.mongodb.net/test?retryWrites=true',
+//   { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Successfully connected to MongoDB Atlas!');
+//   })
+//   .catch((error) => {
+//     console.log('Unable to connect to MongoDB Atlas!');
+//     console.error(error);
+//   });
 
-app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   next();
+// });
 
-app.use('/api/cameras', cameraRoutes);
-app.use('/api/teddies', teddyRoutes);
-app.use('/api/furniture', furnitureRoutes);
+// app.use('/images', express.static(path.join(__dirname, 'images')));
 
-module.exports = app;
+// app.use(bodyParser.json());
+
+// app.use('/api/cameras', cameraRoutes);
+// app.use('/api/teddies', teddyRoutes);
+// app.use('/api/furniture', furnitureRoutes);
+
+// module.exports = app;
