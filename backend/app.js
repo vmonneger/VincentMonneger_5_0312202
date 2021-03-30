0400e67804c2cb2f7aa6305542ecbda91d063e8b
Vince <mvincem@gmail.com>
2021-03-30 10:43:34 +0200

    restructuration du code

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
+import error404Screen from "./error404Screen.js";
 import homeScreen from "./homeScreen.js"
+import productScreen from "./productScreen.js"
+import { parseRequestUrl } from "./utils.js";
 
 // const express = require('express');
 // const bodyParser = require('body-parser');
@@ -10,12 +13,23 @@ import homeScreen from "./homeScreen.js"
 // const furnitureRoutes = require('./routes/furniture');
 
 // const app = express();
-
-const router = () => {
-  const main = document.getElementById("list-product")
-  main.innerHTML = homeScreen.render();
+const routes = {
+  "/": homeScreen,
+  "/product/:id": productScreen,
 }
-window.addEventListener("load", router)
+const router = () => {
+  const request = parseRequestUrl();
+  const parseUrl =
+  (request.ressource ? `/${request.resource}` : "/") +
+  (request.id ? "/:id" : "") +
+  (request.verb ? `/${request.verb}` : "");
+  const screen = routes[parseUrl] ? routes[parseUrl] : error404Screen;
+
+  const main = document.getElementById("list-product")
+  main.innerHTML = screen.render();
+}
+window.addEventListener("load", router);
+window.addEventListener("hashchange", router);
 
 // mongoose.connect(
 //   'mongodb+srv://will:nAcmfCoHGDgzrCHG@cluster0-pme76.mongodb.net/test?retryWrites=true',
