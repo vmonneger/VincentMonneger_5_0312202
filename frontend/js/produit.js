55284aa72c28b8f9a6a73657b498c36c55d37946
Vince <mvincem@gmail.com>
2021-04-09 11:26:20 +0200

    page produit dynamique

--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -27,29 +27,22 @@ class Boutique {
         const findProduit = this.produits.find( object => object._id === urlId);
         theProduct.innerHTML = 
             `
-            <div class=" col-sm-12 col-lg-6">
+            
               <img src="${findProduit.imageUrl}"  class="product-img img-fluid" alt="vcam_1">
-            </div>
-            <div class="container col-sm-12 col-lg-6 text-center">
+            
+            <div class="col-sm-12 col-lg-6 text-center w-100">
               <h1 class="product-name">${findProduit.name}</h1>
               <p class="product-description">${findProduit.description}</p>
               <p class="product-price fw-bold">${findProduit.price}</p>
+            </div>  
               `
-              
-        // let render = findProduit.lenses.forEach(option => {
-        //     select.innerHTML +=`
-        //     <select class="form-select mb-5" id="select" aria-label="Default select example">
-        //         <option selected>Open this select menu</option>
-        //         <option value="1">${option}</option>
-        //         <option value="2">Two</option>
-        //         <option value="3">Three</option>
-        //       </select>
-        //     `
-        //     console.log(render)
-        // })
+        select.innerHTML = findProduit.lenses.map((option, index) => {
+            console.log(option)
+            return`<option value="${index + 1}">${option}</option>`
+        }).join('')
         
-        let options = findProduit.lenses;
-        let ari = options.forEach(element => select.innerHTML = element);
+        // let options = findProduit.lenses;
+        // let ari = options.forEach(element => select.innerHTML = element);
         
         
     }    
@@ -59,6 +52,7 @@ class Boutique {
     }
 }
 
+select.innerHTML = "bonjour"
 
 const boutique = new Boutique();
 
